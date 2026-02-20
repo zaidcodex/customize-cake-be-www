6c07870bf76b0e843bc27f74dc55fa7dfc280751
zaidcodex <zaidalicodex@gmail.com>
2026-02-21 02:34:40 +0500

    push

--- db.js
+++ db.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose')
 
 const URI = 'mongodb+srv://customcake_db_user:LQJrkga3KJ96Q6Zg@customise-cake.5e9d3n1.mongodb.net/?appName=customise-cake'
 
-mongoose.set("strictQuery", false);
-const connectToDB = () => mongoose.connect(URI)
 
-const connectToMongo = async () => {
-  try {
-    await connectToDB();
+const connectToMongo = () => mongoose.connect(URI, () => {
+  
     console.log("Connected to Mongo Successfully");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
-  }
-};
+ })
+
 
 
 module.exports = connectToMongo
